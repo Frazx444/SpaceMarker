74449bc9ff1e1f23d540b6509442354b4cca0baa
Mateus Lora <loramateusatitus@gmail.com>
2024-05-30 16:16:17 -0300

    Adicionei um áudio de fundo e também um quando o usuário clicar na tela.

--- main.py
+++ main.py
@@ -7,10 +7,17 @@ pygame.display.set_caption("Space Marker")
 branco  = (255,255,255) #tupla - definindo a cor da janela
 estrelas = pygame.image.load("assets/starsJW.png")
 
+clicksound = pygame.mixer.Sound("assets/ui-click-43196.mp3") #Var carregando som do Click
+
+pygame.mixer.music.load("assets/space-sound-hi-low-109574.mp3") #Música de fund
+pygame.mixer.music.play(-1)
+
 while True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT: #fechar o jogo
             quit() #comando para fechar o jogo
+        elif evento.type == pygame.MOUSEBUTTONDOWN: #Se botão do mouse for press
+            pygame.mixer.Sound.play(clicksound) #Play som do click
     
     tela.fill(branco) #cor
     tela.blit(estrelas, (0,0))
