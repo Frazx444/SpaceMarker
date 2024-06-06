e7ecdcbea65394cc3f163a0e99f8d4b726f7a3f5
Mateus Lora <loramateusatitus@gmail.com>
2024-06-06 11:33:53 -0300

    Adicionei a escrita em arquivo, mapiei o botão F10, e dei uma breve organizada no código

--- main.py
+++ main.py
@@ -2,29 +2,34 @@ import pygame #importando lib do pygame
 from tkinter import simpledialog
 
 pygame.init() #inicialização de recursos
-tamanho  = (800, 600) #Variável - tamanho da tela
+
 clock = pygame.time.Clock() #frames por segundo
-tela = pygame.display.set_mode(tamanho) #tamanho da tela
-
-fundo = pygame.image.load("assets/bg.jpg")
-#pontoEstrela = pygame.image.load("assets/staricon.png")
-pygame.display.set_caption("Space Marker")
 icone = pygame.image.load("assets/space.png") 
+fundo = pygame.image.load("assets/bg.jpg")
+
+tamanho  = (1000, 563) #Variável - tamanho da tela
+tela = pygame.display.set_mode(tamanho) #tamanho da tela
+pygame.display.set_caption("Space Marker")
 pygame.display.set_icon(icone)
-
-branco  = (255,255,255) #tupla - definindo a cor da janela
-estrelas = {}
-
 fonte = pygame.font.SysFont("comicsans",14)
-
 clicksound = pygame.mixer.Sound("assets/ui-click-43196.mp3") #Var carregando som do Click
-
 pygame.mixer.music.load("assets/space-sound-hi-low-109574.mp3") #Música de fund
 pygame.mixer.music.play(-1)
+
+branco  = (255,255,255) #tupla - definindo a cor da janela
 
 tela.fill(branco) #cor
 tela.blit(fundo, (0,0))
 
+texto1 = fonte.render('Pressione F10 para Salvar os Pontos',True, branco)
+texto2 = fonte.render('Pressione F11 para Carregar os Pontos',True, branco)
+texto3 = fonte.render('Pressione F12 para Deletar os Pontos', True, branco)
+
+tela.blit(texto1, (10, 10))
+tela.blit(texto2, (10, 30))
+tela.blit(texto3, (10, 50))
+ 
+estrelas = {}
 while True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT: #fechar o jogo
@@ -37,20 +42,24 @@ while True:
             nome = simpledialog.askstring('space', 'Digite o nome da estrela: ')
             if not nome:
                 nome = "desconhecido"+str(posicao)
-            estrela = fonte.render(nome, True, branco)
+           
             estrelas[nome] = posicao
+            print(estrelas)
+
+            estrela = fonte.render(nome, True, branco)
             tela.blit(estrela, (posicao))
-     
-    
-    texto1 = fonte.render('Pressione F10 para Salvar os Pontos',True, branco)
-    texto2 = fonte.render('Pressione F11 para Carregar os Pontos',True, branco)
-    texto3 = fonte.render('Pressione F12 para Deletar os Pontos', True, branco)
 
-    tela.blit(texto1, (10, 10))
-    tela.blit(texto2, (10, 30))
-    tela.blit(texto3, (10, 50))
+        elif evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_F10:  
+                try:
+                    arquivo = open("pontossalvos.txt","r",encoding="utf-8")
+                    estrelas = eval(arquivo.write())
+                    arquivo.close()
 
-    pygame.display.update() #novos eventos na tela
-    clock.tick(60) #atualização da tela 
-
-pygame.quit()
+                except:
+                    arquivo = open("pontossalvos.txt","w",encoding="utf-8")
+                    arquivo.write(str(estrelas))
+                    arquivo.close()
+                
+        pygame.display.update() #novos eventos na tela
+        clock.tick(60) #atualização da tela
